897ff194d352419978fa095a8d99b5f5e4669047
PerikiyoXD <pedroluis200198@hotmail.es>
2017-05-07 10:37:41 +0200

    Update rot13.cpp

--- rot13.cpp
+++ rot13.cpp
@@ -6,23 +6,21 @@
 
 std::string rot13(std::wstring text_to_decode)
 {
-    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
-    std::string text = converter.to_bytes(text_to_decode);
-    std::cout << "Text to decode: \"" << text << "\"" << std::endl;
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+	std::string text = converter.to_bytes(text_to_decode);
 	std::string final_result;
 	for ( std::string::iterator it=text.begin(); it!=text.end(); ++it)
-    {
-		if (! isalpha(*it))
-		{
-			final_result.push_back(*it);
+    	{
+		if (isalpha(*it))
+		{	
+			bool is_lowcase = islower(*it);
+    			final_result.push_back((is_lowcase == true ? 97 : 65) + (*it - (is_lowcase == true ? 97 : 65) + 13) % 26);
 		}
 		else
 		{
-		    bool is_lowcase = islower(*it);
-    		std::cout << *it << ": Is lowcase: " << is_lowcase << std::endl;
-    		final_result.push_back((is_lowcase == true ? 97 : 65) + (*it - (is_lowcase == true ? 97 : 65) + 13) % 26);
-	    }
-    }
+			final_result.push_back(*it);
+	    	}
+    	}
 	std::cout << std::endl;
 	return final_result;
 }
